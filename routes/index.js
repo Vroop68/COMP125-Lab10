19388ca214e4004eb21d4d5229616f800ff2b5de
John Knoop <john.r.knoop@gmail.com>
2018-08-20 17:29:06 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+let express = require("express");
+let router = express.Router();
+
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  res.render("content/index", {
+    title: "Express Demo with Heroku!"
+  });
+});
+
+/* GET about page. */
+router.get("/about", function(req, res, next) {
+  res.render("content/about", {
+    title: "About Express",
+    author: "John Knoop"
+  });
+});
+
+/* GET contact page. */
+router.get("/contact", function(req, res, next) {
+  res.render("content/contact", {
+    title: "Contact Me",
+    authors: ["John", "Steve", "Erich", "Danny", "Ashley"]
+  });
+});
+
+module.exports = router;
